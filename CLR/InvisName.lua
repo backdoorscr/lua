90c2f32341c3b2b637ef650a2b69dc48ce44213c
backdoorscr <untracedbackdoor@gmail.com>
2025-09-06 15:16:25 -0500

    Create InvisName.lua

---
+++ CLR/InvisName.lua
@@ -0,0 +1,29 @@
+-- Generated with Sigma Spy Github: https://github.com/depthso/Sigma-Spy
+-- Remote
+local ChangeNameEvent = workspace.EventsRPGUI.ChangeNameEvent -- RemoteEvent 
+
+ChangeNameEvent:FireServer(
+    [[  o         o           o           o__ __o     o         o/   o__ __o__/_   o__ __o            o__ __o  \o       o/         o__ __o      o         o    o__ __o     o          o    o__ __o__/_   o__ __o    ____o__ __o____ 
+
+ <|>       <|>         <|>         /v     v\   <|>       /v   <|    v       <|     v\          <|     v\  v\     /v         /v     v\    <|>       <|>  <|     v\   <|\        /|>  <|    v       <|     v\    /   \   /   \  
+
+ < >       < >         / \        />       <\  / >      />    < >           / \     <\         / \     <\  <\   />         />       <\   / \       / \  / \     <\  / \\o    o// \  < >           / \     <\        \o/       
+
+  |         |        o/   \o    o/             \o__ __o/       |            \o/       \o       \o/     o/    \o/          _\o____        \o/       \o/  \o/     o/  \o/ v\  /v \o/   |            \o/     o/         |        
+
+  o__/_ _\__o       <|__ __|>  <|               |__ __|        o__/_         |         |>       |__  _<|      |                \_\__o__   |         |    |__  _<|/   |   <\/>   |    o__/_         |__  _<|         < >       
+
+  |         |       /       \   \\              |      \       |            / \       //        |       \    / \                     \   < >       < >   |          / \        / \   |             |       \         |        
+
+ <o>       <o>    o/         \o   \         /  <o>      \o    <o>           \o/      /         <o>      /    \o/           \         /    \         /   <o>         \o/        \o/  <o>           <o>       \o       o        
+
+  |         |    /v           v\   o       o    |        v\    |             |      o           |      o      |             o       o      o       o     |           |          |    |             |         v\     <|        
+
+ / \       / \  />             <\  <\__ __/>   / \        <\  / \  _\o__/_  / \  __/>          / \  __/>     / \            <\__ __/>      <\__ __/>    / \         / \        / \  / \  _\o__/_  / \         <\    / \       
+
+                                                                                                                                                                                                                              
+
+                                                                                                                                                                                                                              
+
+                                                                                                                                                                                                                              ]]
+)
