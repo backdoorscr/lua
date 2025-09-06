7b78b3a082a0372288bbf8e11d7a7778e00ca78a
backdoorscr <untracedbackdoor@gmail.com>
2025-09-06 15:45:41 -0500

    Create InvisibleBio.lua

---
+++ CLR/InvisibleBio.lua
@@ -0,0 +1,42 @@
+-- Generated with Sigma Spy Github: https://github.com/depthso/Sigma-Spy
+-- Remote
+local ChangeBioEvent = workspace.EventsRPGUI.ChangeBioEvent -- RemoteEvent 
+
+ChangeBioEvent:FireServer(
+    [[             
+             
+             
+    o__ __o/ 
+   /v     |  
+  />     / \ 
+  \      \o/ 
+   o      |  
+   <\__  / \ 
+             
+             
+             
+             
+             
+             
+     __o__   
+    />  \    
+    \o       
+     v\      
+      <\     
+ _\o__</     
+             
+             
+             
+             
+             
+             
+     __o__   
+    />  \    
+    \o       
+     v\      
+      <\     
+ _\o__</     
+             
+             
+             ]]
+)
