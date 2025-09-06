600246016bedb2cbfd57e0112dbc6db3a693a488
backdoorscr <untracedbackdoor@gmail.com>
2025-09-06 15:21:38 -0500

    Create BuggedBio.lua

---
+++ CLR/BuggedBio.lua
@@ -0,0 +1,29 @@
+-- Generated with Sigma Spy Github: https://github.com/depthso/Sigma-Spy
+-- Remote
+local ChangeBioEvent = workspace.EventsRPGUI.ChangeBioEvent -- RemoteEvent 
+
+ChangeBioEvent:FireServer(
+    [[ __o__                                                                                   o              
+
+   |                                                                                   _<|>_            
+
+  / \                                                                                                   
+
+  \o/          o__ __o/  \o__ __o__ __o       \o__ __o__ __o    o       o       __o__    o        __o__ 
+
+   |          /v     |    |     |     |>       |     |     |>  <|>     <|>     />  \    <|>      />  \  
+
+  < >        />     / \  / \   / \   / \      / \   / \   / \  < >     < >     \o       / \    o/       
+
+   |         \      \o/  \o/   \o/   \o/      \o/   \o/   \o/   |       |       v\      \o/   <|        
+
+   o          o      |    |     |     |        |     |     |    o       o        <\      |     \\       
+
+ __|>_        <\__  / \  / \   / \   / \      / \   / \   / \   <\__ __/>   _\o__</     / \     _\o__</ 
+
+                                                                                                        
+
+                                                                                                        
+
+                                                                                                        ]]
+)
