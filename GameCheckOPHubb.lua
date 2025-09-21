c29389fdddeda9e068fbb532a09b601b5ba3590f
backdoorscr <untracedbackdoor@gmail.com>
2025-09-21 13:48:58 -0500

    Update GameCheckOPHubb.lua

--- GameCheckOPHubb.lua
+++ GameCheckOPHubb.lua
@@ -1,1 +1,12 @@
+local placeId = game.PlaceId
+print(placeId)
 
+if placeId == 129907376296872 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/backdoorscr/lua/refs/heads/main/OPHubb.lua"))()
+elseif placeId == 110113229454709 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/backdoorscr/lua/refs/heads/main/Loaded.lua"))()
+elseif placeId == 127805224534849 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/backdoorscr/Loader/refs/heads/main/labubushit.lua"))()
+else
+    warn("Game not supported!")
+end
